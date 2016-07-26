05f3b9fc3f64a526f23d961490577ad883e2d457
andyafter <andyafter@gmail.com>
2016-07-26 17:49:28 +0800

    Added layouts

---
+++ latex/slides/auto/presentation.el
@@ -0,0 +1,50 @@
+(TeX-add-style-hook
+ "presentation"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("beamer" "bigger")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "beamer"
+    "beamer10"
+    "inputenc"
+    "fontenc"
+    "fixltx2e"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref")
+   (LaTeX-add-labels
+    "sec:orgheadline2"
+    "sec:orgheadline1"
+    "sec:orgheadline12"
+    "sec:orgheadline3"
+    "sec:orgheadline5"
+    "sec:orgheadline4"
+    "sec:orgheadline8"
+    "sec:orgheadline6"
+    "sec:orgheadline7"
+    "sec:orgheadline11"
+    "sec:orgheadline9"
+    "sec:orgheadline10"
+    "sec:orgheadline14"
+    "sec:orgheadline13"))
+ :latex)
+
