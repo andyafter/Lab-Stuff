4a0cdb9cb1febc1f9ade3e3408a0fe965d33396f
VR_room <VR_room>
2016-07-27 17:57:55 +0800

    (web) Changed the localhost to public.

--- website/app/home/home.js
+++ website/app/home/home.js
@@ -1,13 +1,17 @@
-angular.module('myApp.home', ['ngRoute'])
+angular.module('myApp.home',
+               ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/home', {
         templateUrl: 'home/home.html',
         controller: 'HomeController'
     });
 }])
-.controller('HomeController', [function() {
+.controller('HomeController', [function($scope) {
     console.log("something is happening");
-    // this is for the home page of the website.    
+    // this is for the home page of the website.
+    $scope.init = function(){
+        console.log("Something is definitely happening!");
+    }
 }]);
 
 
