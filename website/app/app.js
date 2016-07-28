c294f5251f19a282afee2076f5daff3ac44476f9
andyafter <andyafter@gmail.com>
2016-07-28 16:46:11 +0800

    (Web) I decided to use simplified version of router.

--- website/app/app.js
+++ website/app/app.js
@@ -3,15 +3,30 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
     'ngRoute',
+    'ui.router',
     'myApp.home',
     'myApp.view1',
     'myApp.view2',
     'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-    $locationProvider.hashPrefix('!');
-    $routeProvider.otherwise({redirectTo: '/view1'});
-    
-
-
-}]);
+])
+.config(['$locationProvider',
+         '$routeProvider',
+         function($locationProvider, $routeProvider,
+                  $stateProvider, $urlRouterProvider, $interpolateProvider,
+                  $httpProvider, $resourceProvider) {
+             $locationProvider.hashPrefix('!');
+             $routeProvider.otherwise({redirectTo: '/home'});
+             $locationProvider.html5Mode(true);
+             
+}])
+    .controller('AppController',
+                function AppController($scope, $rootScope, $location,
+                                       $http, $log, $stateParams, $state){
+                    
+                    $scope.message = "I am just testing ";
+                    $scope.init = function(){
+                        console.log("Here is something you might be interested!");
+                    }
+        
+    })
+;
--- website/app/home/home.js
+++ website/app/home/home.js
@@ -1,18 +1,23 @@
 angular.module('myApp.home',
-               ['ngRoute'])
-.config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/home', {
-        templateUrl: 'home/home.html',
-        controller: 'HomeController'
-    });
-}])
-.controller('HomeController', [function($scope) {
-    console.log("something is happening");
-    // this is for the home page of the website.
-    $scope.init = function(){
-        console.log("Something is definitely happening!");
-    }
-}]);
-
-
-
+               ['myApp',
+                'ngRoute',
+                'ui.router',])
+    .config(['$routeProvider',
+             function($routeProvider) {
+                 $routeProvider
+                     .when('/', {
+                         templateUrl : 'home/home.html',
+                         controller  : 'HomeController'
+                     })
+                     .when('/home', {
+                         templateUrl: 'home/home.html',
+                         controller: 'HomeController'
+                     });
+             }])
+    .controller('HomeController', ['$scope',function($scope) {
+        // this is for the home page of the website.
+        $scope.init = {};
+        $scope.init = function() {
+            console.log("something is happening");
+        };
+    }]);
