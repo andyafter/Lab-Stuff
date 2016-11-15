e6b5fb9747bae0d941089f2c8eb657a9bb313a79
andyafter <andyafter@gmail.com>
2016-11-15 15:48:50 +0800

    Code example for ARTClient

--- project/BackEngine/src/artdummy.cpp
+++ project/BackEngine/src/artdummy.cpp
@@ -4,8 +4,11 @@
 #include<string>
 #include<thread>
 
+
 ARTDummy::ARTClient(){
   std::cout << "ARTDummy Initialized!" << std::endl;
-  
 }
 
+void ARTDummy::startUDPServer(){
+  std::cout << "UDP Dummy Server Started!" << std::endl;
+}
