93bd2375eac884e8de3f3e6ec52e4dcc264f1f18
andyafter <andyafter@gmail.com>
2016-11-22 14:19:51 +0800

    Latex report for d9 and d10

---
+++ latex/report/auto/master.el
@@ -0,0 +1,19 @@
+(TeX-add-style-hook
+ "master"
+ (lambda ()
+   (TeX-run-style-hooks
+    "setup/preamble"
+    "setup/hyphenations"
+    "setup/macros"
+    "sections/frontpage"
+    "sections/colophon"
+    "sections/titlepages"
+    "sections/preface"
+    "sections/introduction"
+    "sections/ch2name"
+    "sections/conclusion"
+    "sections/appAname")
+   (LaTeX-add-labels
+    "bib:mybiblio"))
+ :latex)
+
