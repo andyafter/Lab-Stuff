42377a5581aed7a37584af6f962f2e0cae08fd3b
andyafter <andyafter@gmail.com>
2016-12-15 14:14:21 +0800

    Reveived art data with the right address

--- project/GesEngine/artclient.cpp
+++ project/GesEngine/artclient.cpp
@@ -1,8 +1,10 @@
 #include "artclient.h"
 
 ARTClient::ARTClient(QObject *parent) : QObject(parent){
+    QHostAddress *art = new QHostAddress("192.168.1.110");  // art address
+
     socket = new QUdpSocket(this);
-    socket->bind(QHostAddress::LocalHost,5002);
+    socket->bind(*art,5002);
     connect(socket, SIGNAL(readyRead()),this,SLOT(readyRead()));
 }
 
@@ -10,8 +12,8 @@ void ARTClient::readyRead(){
     QByteArray Buffer;
     Buffer.resize(socket->pendingDatagramSize());
 
-    QHostAddress *sender = new QHostAddress("127.0.0.1");
-    quint16 senderPort = 5000;
+    QHostAddress *sender = new QHostAddress("192.168.1.110");
+    quint16 senderPort = 5002;
     socket->readDatagram(Buffer.data(),Buffer.size(), sender, &senderPort);
     qDebug()<<"message from" <<sender->toString();
     qDebug()<<"message port" <<senderPort;
