46932d28b021190b81a4e59ff35dbd5e9c2e989c
andyafter <andyafter@gmail.com>
2016-12-27 11:13:03 +0800

    kmeans for ART data

--- project/analysis/features.py
+++ project/analysis/features.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.cluster import KMeans
 
 import time
 
@@ -11,3 +12,11 @@ data.get_data("./data/data.txt")
 
 s = data.marker_data[-1]
 print s
+
+X = np.array(s)
+
+kmeans = KMeans(n_clusters=7, random_state=0).fit(X)
+
+prediction = kmeans.fit_predict(X)
+
+
