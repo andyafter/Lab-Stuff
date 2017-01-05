b8a4105036332b3eb761e2603a738f2c109855d0
andyafter <andyafter@gmail.com>
2017-01-05 13:52:17 +0800

    xaxis

--- project/analysis/noise.py
+++ project/analysis/noise.py
@@ -8,6 +8,14 @@ import time
 from animation import AnimatedScatter
 
 
-data = AnimatedScatter()
-data.get_data("./gesdata/data15.txt")
 
+for ndata in range(2,21):
+    data = AnimatedScatter()
+    data.get_data("./gesdata/data{}.txt".format(str(ndata)))
+
+    n = 0
+    plt.clf()
+    for frame in data.marker_data:
+        plt.scatter([n]*len(frame), np.array(frame)[:, 0])
+        n += 1
+    plt.savefig("data{}xaxis.png".format(str(ndata)))
