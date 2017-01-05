e940238487c7342af30ce8cf217e74c71056d39c
andyafter <andyafter@gmail.com>
2017-01-05 13:54:07 +0800

    to pull

--- project/GesEngine/main.cpp
+++ project/GesEngine/main.cpp
@@ -2,6 +2,8 @@
 #include <QApplication>
 #include <iostream>
 
+#include "artclient.h"
+
 using namespace std;
 
 int main(int argc, char *argv[])
@@ -9,8 +11,6 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-
-    cout<< "something something" << endl;
 
     return a.exec();
 }
