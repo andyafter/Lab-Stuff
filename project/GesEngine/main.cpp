ce8b690c91a50b15bb28ef7fb4374f39a52e69ca
andyafter <andyafter@gmail.com>
2017-01-16 10:33:43 +0800

    Deleted thread test

--- project/GesEngine/main.cpp
+++ project/GesEngine/main.cpp
@@ -8,9 +8,15 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
+    /*
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
 
     return a.exec();
+    */
+    QVector<float> s;
+    QString rawData = "";
+    qDebug()<<s;
+    return 0;
 }
