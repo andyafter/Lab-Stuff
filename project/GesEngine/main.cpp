5d418405d06507bb23783ac48b9854a0db5e7c92
andyafter <andyafter@gmail.com>
2017-01-16 10:38:06 +0800

    Added something else

--- project/GesEngine/main.cpp
+++ project/GesEngine/main.cpp
@@ -8,15 +8,9 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    /*
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
 
     return a.exec();
-    */
-    QVector<float> s;
-    QString rawData = "";
-    qDebug()<<s;
-    return 0;
 }
