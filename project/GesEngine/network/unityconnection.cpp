84251b08bab00817841db09f68b54f26dd498908
andyafter <andyafter@gmail.com>
2017-01-19 15:53:18 +0800

    functions in unityconnection

--- project/GesEngine/network/unityconnection.cpp
+++ project/GesEngine/network/unityconnection.cpp
@@ -4,3 +4,33 @@ UnityConnection::UnityConnection(QObject *parent) : QObject(parent)
 {
 
 }
+
+void UnityConnection::setSocket(QTcpSocket *socket)
+{
+
+}
+
+void UnityConnection::connected()
+{
+
+}
+
+void UnityConnection::disconnected()
+{
+
+}
+
+void UnityConnection::byteWritten(qint64)
+{
+
+}
+
+void UnityConnection::stateChanged(QAbstractSocket::SocketState socketState)
+{
+
+}
+
+void UnityConnection::error(QAbstractSocket::SocketError socketError)
+{
+
+}
--- project/GesEngine/network/unityconnection.h
+++ project/GesEngine/network/unityconnection.h
@@ -2,16 +2,29 @@
 #define UNITYCONNECTION_H
 
 #include <QObject>
+#include <QTcpSocket>
 
 class UnityConnection : public QObject
 {
     Q_OBJECT
 public:
     explicit UnityConnection(QObject *parent = 0);
+    ~UnityConnection();
+
+    virtual void setSocket(QTcpSocket *socket);
 
 signals:
 
+protected:
+    QTcpSocket *m_socket;
+    QTcpSocket *get_socket;
+
 public slots:
+    virtual void connected();
+    virtual void disconnected();
+    virtual void byteWritten(qint64);
+    virtual void stateChanged(QAbstractSocket::SocketState socketState);
+    virtual void error(QAbstractSocket::SocketError socketError);
 };
 
 #endif // UNITYCONNECTION_H
