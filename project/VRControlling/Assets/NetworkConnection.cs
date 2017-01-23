905b4ab320dc55dbc43f3930dcf191e580b8c4b1
andyafter <andyafter@gmail.com>
2017-01-23 13:02:48 +0800

    Added network communication to vrcontrolling

---
+++ project/VRControlling/Assets/NetworkConnection.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using SharpConnect;
+
+public class NetworkConnection : MonoBehaviour {
+    public Connector test=new Connector();
+    string lastMessage;
+    public Transform PlayerCoord;
+ 
+    void Start () 
+    {
+        Debug.Log(test.fnConnectResult("127.0.0.1",12345,System.Environment.MachineName));
+        if (test.res !="")
+        {
+            Debug.Log(test.res);
+        }
+    }
+    void Update () 
+    {
+        if(test.strMessage!=""){
+            print(test.strMessage);
+            // this is kinda dirty. It helps clears all the message from the server
+            // every time there is an update
+            test.strMessage = "";
+        }
+    }
+ 
+    void OnApplicationQuit (){
+        try{test.fnDisconnect();}
+        catch{
+            print("disconnect error catched");
+        }
+    }
+}
