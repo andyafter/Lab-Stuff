92f5ab54086a69b9147b9d56faacb334da7d6c39
andyafter <andyafter@gmail.com>
2017-03-02 14:40:07 +0800

    Added main function

--- project/RobotDQN/robot.py
+++ project/RobotDQN/robot.py
@@ -2,6 +2,7 @@ import sys
 import numpy as np
 
 class Robot(self):
+    
     def __init__(self):
         # legal actions
         self.actions = []
---
+++ project/RobotDQN/run_dqn.py
@@ -0,0 +1,11 @@
+import sys
+
+from robot import *
+
+
+def main():
+    print "Starting Robot"
+
+if __name__ == '__main__':
+    main()
+
