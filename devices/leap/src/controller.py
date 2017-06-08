f9b692f65e7384caca12b509203b6916b539e33a
andyafter <andyafter@gmail.com>
2017-06-08 15:46:21 +0800

    Leap Motion with UDP

--- devices/leap/src/controller.py
+++ devices/leap/src/controller.py
@@ -1,4 +1,4 @@
-import os, sys, inspect, thread, time
+import os, sys, inspect, thread, time, socket
 src_dir = os.path.dirname(inspect.getfile(inspect.currentframe()))
 #arch_dir = './lib/x64' if sys.maxsize > 2**32 else './lib/x86'
 arch_dir = '../lib'
@@ -11,12 +11,17 @@ from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
 class LeapFlaskListener(Leap.Listener):
 
     def on_connect(self, controller):
+        self.UDP_IP = '0.0.0.0'
+        self.UDP_PORT = 5112
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         print "Connected"
 
     def on_frame(self, controller):
+        self.fingers = []
+        
         frame = controller.frame()
         #print "Frame id: %d, timestamp: %d, hands: %d, fingers: %d" % (
-          #frame.id, frame.timestamp, len(frame.hands), len(frame.fingers))
+        #frame.id, frame.timestamp, len(frame.hands), len(frame.fingers))
         pointable = frame.pointables.frontmost
         direction = pointable.direction
         length = pointable.length
@@ -26,7 +31,11 @@ class LeapFlaskListener(Leap.Listener):
         speed = pointable.tip_velocity
         touchDistance = pointable.touch_distance
         zone = pointable.touch_zone
-        print zone, position
+        for point in frame.pointables:
+            # point here is a pointable
+            self.fingers.append(point)
+        MESSAGE = str(len(self.fingers))
+        self.sock.sendto(MESSAGE, (self.UDP_IP, self.UDP_PORT))
 
 
 def main():
@@ -46,3 +55,5 @@ def main():
     finally:
         # Remove the sample listener when done
         controller.remove_listener(listener)
+
+main()
--- devices/leap/src/server.py
+++ devices/leap/src/server.py
@@ -5,29 +5,45 @@ from flask import Flask
 app = Flask(__name__)
 
 import sys, time
+import threading
+import json
+
 from controller import LeapFlaskListener, Leap
 
+pointables = []
 
-listener = LeapFlaskListener()
-controller = Leap.Controller()
 
-# Have the sample listener receive events from the controller
-controller.add_listener(listener)
+def listen():
+    # Have the sample listener receive events from the controller
+    
+    listener = LeapFlaskListener()
+    controller = Leap.Controller()
 
-# Keep this process running until Enter is pressed
-print "Press Enter to quit..."
-try:
-    sys.stdin.readline()
-except KeyboardInterrupt:
-    pass
-finally:
-    # Remove the sample listener when done
-    controller.remove_listener(listener)
+    controller.add_listener(listener)
+
+    # Keep this process running until Enter is pressed
+    print "Press Enter to quit..."
+    try:
+        sys.stdin.readline()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        # Remove the sample listener when done
+        controller.remove_listener(listener)
+
+#t = threading.Thread(target=listen)
+#t.start()
 
 
 @app.route('/')
 def hello_world():
     return 'Success!'
+
+
+@app.route('/fingers')
+def get_fingers():
+    return str(len(pointables))
+
 
 if __name__ == "__main__":
     app.run()
